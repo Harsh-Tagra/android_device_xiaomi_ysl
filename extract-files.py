bff80809cb6e885eb87583fa5bbca2e27df94b13
Giovanni Ricca <giovanniricca@proton.me>
2024-11-15 23:33:24 +0100

    ysl: Migrate to py extract utils
    Change-Id: Ic7a71d021a2436e897496ba2c16ebfeda7e3c383

---
+++ extract-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib/libarcsoft_high_dynamic_range.so', 'vendor/lib/libremosaic_wrapper.so', 'vendor/lib/libremosaiclib.so', 'vendor/lib/libmmcamera_hdr_gb_lib.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib/libgf_hal.so': blob_fixup()
+        .sig_replace('00 c6 8f e2 4a ca 8c e2 b0 fa bc e5', '00 c6 8f e2 1f 20 03 d5 b0 fa bc e5')
+        .sig_replace('78 47 c0 46 00 c0 9f e5 0f f0 8c e0 e0 37 fc ff', '78 47 c0 46 1f 20 03 d5 0f f0 8c e0 e0 37 fc ff'),
+    'vendor/lib/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
+        .remove_needed('libprotobuf-cpp-lite.so'),
+    'vendor/lib64/hw/consumerir.msm8953.so': blob_fixup()
+        .binary_regex_replace(b'/dev/spidev6.1', b'/dev/spidev5.1')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'ysl',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'msm8953-common', module.vendor)
+    utils.run()
