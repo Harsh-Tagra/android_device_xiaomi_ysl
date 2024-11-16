6e85b8453bab44296add35a643d72d3ca9c3dd7e
Giovanni Ricca <giovanniricca@proton.me>
2024-11-16 11:57:10 +0100

    ysl: Fix fingerprint.goodix.default ELF checks
    Change-Id: If25ff641ad9383a71553bb2cb0548133c362f7c8

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_18'
+
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -20,6 +24,8 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/hw/fingerprint.goodix.default.so': blob_fixup()
+        .replace_needed('libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor.goodix.hardware.fingerprint@1.0.so'),
     ('vendor/lib/libarcsoft_high_dynamic_range.so', 'vendor/lib/libremosaic_wrapper.so', 'vendor/lib/libremosaiclib.so', 'vendor/lib/libmmcamera_hdr_gb_lib.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib/libgf_hal.so': blob_fixup()
@@ -27,7 +33,7 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('78 47 c0 46 00 c0 9f e5 0f f0 8c e0 e0 37 fc ff', '78 47 c0 46 1f 20 03 d5 0f f0 8c e0 e0 37 fc ff'),
     'vendor/lib/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
         .remove_needed('libprotobuf-cpp-lite.so')
-        .binary_regex_replace(b'libvendor.goodix.hardware.fingerprint@1.0.so', b'vendor.goodix.hardware.fingerprint@1.0.so\x00\x00\x00'),
+        .replace_needed('libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor.goodix.hardware.fingerprint@1.0.so'),
     'vendor/lib64/hw/consumerir.msm8953.so': blob_fixup()
         .binary_regex_replace(b'/dev/spidev6.1', b'/dev/spidev5.1')
 }  # fmt: skip
